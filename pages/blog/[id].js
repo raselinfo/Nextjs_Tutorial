38fc26d1ee977842db15c6ba1281689df6311dd8
raselinfo <raselinfo52@gmail.com>
2022-04-19 12:22:30 -0700

    NextJS Head And Script Component

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,11 +1,35 @@
-import React from 'react';
+import Head from "next/head"
+import Script from "next/script"
 import { useRouter } from 'next/router';
+import { Html } from "next/document"
+import { useState } from "react"
 const Blog = () => {
     const router = useRouter()
     const { id } = router.query
+    const [name, setName] = useState("Rasel")
     return (
-        <div>
-            <h1>Hello Blog {id}</h1>
+        <div id="banner" className="rasel">
+
+            <Head>
+                <title >Blog Page</title>
+                {/* <script src="/index.js"></script> */}
+            </Head>
+            <Script id="banner" src="/index.js" strategy="lazyOnload"
+                onLoad={
+                    () => {
+                        window.a = "Rasel"
+                        setName("Rabina")
+                        console.log("Onload")
+                    }
+                }
+                onError={
+                    (error) => console.log("My Error",error)
+                }
+            >
+
+            </Script>
+
+            <h1>{name} Hello Blog {id}</h1>
         </div>
     );
 };
