91bff5f1e2ff51c80b107e18cf734bf28e50039e
John Doe <rabinarahman@gmail.com>
2022-04-24 00:53:57 -0700

    Api Call

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Meta from "../components/Meta"
 
-export default function Home() {
+export default function Home({ blogs }) {
+
   return (
     <div className='container mt-5'>
       <Meta title="Home" description="this is home description" keywords="home,blog" />
-      <h1>Index Page</h1>
+      <ul>
+        {blogs.map(blog => {
+          return <li key={blog.id}>
+            <h3>{blog.title.substring(0,10)}</h3>
+            <p>{blog.body.substring(0,50)}</p>
+          </li>
+        })}
+      </ul>
+
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  let res = await fetch("https://jsonplaceholder.typicode.com/posts")
+  let blogs = await res.json()
+  return {
+    props: {
+      blogs
+    }
+  }
+}
+
